0b2652f40c1810a48b10f42cb525b4e49dd73331
david361215 <1138890119@qq.com>
2022-12-06 11:19:36 +0800

    deploy

---
+++ precache-manifest.0d179a5e94fb44f18a280c97503a3230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1816547a541d013040f",
+    "url": "/morney-1-website/css/app.c7735192.css"
+  },
+  {
+    "revision": "0575b8009f427a0c8c569658d0c83953",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "a1816547a541d013040f",
+    "url": "/morney-1-website/js/app.94b05610.js"
+  },
+  {
+    "revision": "b09c4fb964cec9b21c87",
+    "url": "/morney-1-website/js/chunk-vendors.641fdf92.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "d2ae616149b4b9ebf637a5070ece06aa",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
